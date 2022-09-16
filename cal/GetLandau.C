f19693f5b71177c03dfac5cbc70656978eae415c
Garypenman <g.penman.1@research.gla.ac.uk>
2022-09-16 07:29:15 -0400

    Updated GetLandau and added SBSTree class structure

--- cal/GetLandau.C
+++ cal/GetLandau.C
@@ -15,18 +15,26 @@
 #include <TLine.h>
 #include <TLegend.h>
 
+#include "SBSTree.C"
 
 const Int_t nAdc = 64;
 const Int_t nBars = 32;
 const Int_t nSide = 2;
 
-TString REPLAYED_DIR = "/adaqfs/home/a-onl/sbs/Rootfiles/";
-TString ANALYSED_DIR = "/adaqfs/home/a-onl/sbs/Rootfiles/bbhodo_hist";
+const Int_t adcbarstart = 32;
+
+//TString REPLAYED_DIR = "/adaqfs/home/a-onl/sbs/Rootfiles/";
+//TString ANALYSED_DIR = "/adaqfs/home/a-onl/sbs/Rootfiles/bbhodo_hist";
 
 // for local analysis at uog (please leave in comments)
 //TString REPLAYED_DIR = "/w/work0/home/rachel/HallA/BB_Hodo/FallRun2021/Replayed";
 //TString ANALYSED_DIR = "/w/work0/home/rachel/HallA/BB_Hodo/FallRun2021/Analysed";
 
+// for local analysis at jlab (please leave in comments)
+TString REPLAYED_DIR = "/volatile/halla/sbs/gpenman/Rootfiles";
+TString ANALYSED_DIR = "/volatile/halla/sbs/gpenman/Analysed";
+
+/*
 namespace Thodo {
   Int_t NdataAdcBar;
   Double_t ADCBar[nBars];
@@ -36,13 +44,14 @@ namespace Thodo {
   Double_t ADCValR[nBars];
   //  Int_t ADCBarOff[nBars];
 };
+*/
 
-TChain *T = 0;
+TChain *C;
 
 using namespace std;
 
-void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
-	       Double_t FitStart=150.0, Double_t FitStop = 1000.0,
+void GetLandau(const Int_t RunNum=11590, Int_t nevents=-1, bool DoBar=true,
+	       Double_t FitStart=0.0, Double_t FitStop = 1000.0,
 	       Double_t Width=40.0, Int_t HistRange=3000){
   // InFile is the input file without absolute path and without .root suffix
   // nevents is how many events to analyse, -1 for all
@@ -55,52 +64,59 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   // .L GetLandau.C+
   // GetLandau("filename",-1,150.0,1000.0,40.0,3000)
 
-
+  TString prefix, sRunNum, snevents, sInFile;
   //========================================================= Get data from tree
-  if(!T) { 
-    TString sInFile = REPLAYED_DIR + "/" + InFile + ".root";
+  if(!C) { 
+    prefix = "e1209019_bbhodo_";
+    sRunNum = std::to_string(RunNum);
+    snevents = std::to_string(nevents);
+    //TString sInFile = REPLAYED_DIR + "/" + InFile + ".root";
+    sInFile = REPLAYED_DIR + "/" + prefix + sRunNum;
     cout << "Adding " << sInFile << endl;
-    T = new TChain("T");
-    T->Add(sInFile);
+    C = new TChain("T");
+    C->Add(sInFile+"*");
     
     // disable all branches
-    T->SetBranchStatus("*",0);
+    //C->SetBranchStatus("*",0);
     // enable adc branches
-    T->SetBranchStatus("bb.hodoadc.*",1);
-    T->SetBranchAddress("bb.hodoadc.adcbarid",Thodo::ADCBar);
-    T->SetBranchAddress("bb.hodoadc.L.ap",Thodo::ADCValL);
-    T->SetBranchAddress("bb.hodoadc.R.ap",Thodo::ADCValR);
-    // T->SetBranchAddress("bb.hodoadc.adcbaroff",Thodo::ADCBarOff);
+    //C->SetBranchStatus("bb.hodoadc.*",1);
+    //C->SetBranchAddress("bb.hodoadc.bar.adc.id",Thodo::ADCBar);
+    //C->SetBranchAddress("bb.hodoadc.bar.adc.L.ap",Thodo::ADCValL);
+    //C->SetBranchAddress("bb.hodoadc.bar.adc.R.ap",Thodo::ADCValR);
+    // C->SetBranchAddress("bb.hodoadc.adcbaroff",Thodo::ADCBarOff);
     // enable vector size branches
-    T->SetBranchStatus("Ndata.bb.hodoadc.*",1);
-    T->SetBranchAddress("Ndata.bb.hodoadc.adcbarid",&Thodo::NdataAdcBar);
-    T->SetBranchAddress("Ndata.bb.hodoadc.L.ap",&Thodo::NdataAdcL);
-    T->SetBranchAddress("Ndata.bb.hodoadc.R.ap",&Thodo::NdataAdcR);  
+    //C->SetBranchStatus("Ndata.bb.hodoadc.*",1);
+    //C->SetBranchAddress("Ndata.bb.hodoadc.bar.adc.id",&Thodo::NdataAdcBar);
+    //C->SetBranchAddress("Ndata.bb.hodoadc.bar.adc.L.ap",&Thodo::NdataAdcL);
+    //C->SetBranchAddress("Ndata.bb.hodoadc.bar.adc.R.ap",&Thodo::NdataAdcR);  
     
   }//setting tree
   
   
   
   //========================================================= Check no of events
-  Int_t Nev = T->GetEntries();
+  SBSTree *T = new  SBSTree(C);
+  
+  Int_t Nev = C->GetEntries();
   cout << "N entries in tree is " << Nev << endl;
   Int_t NEventsAnalysis;
   if(nevents==-1) NEventsAnalysis = Nev;
   else NEventsAnalysis = nevents;
+  //NEventsAnalysis = 500000;
   cout << "Running analysis for " << NEventsAnalysis << " events" << endl;
   
 
   
   //==================================================== Check the bar offset
-  T->GetEntry(0);
-  Int_t adcbarstart = (Int_t)Thodo::ADCBar[0];
-  cout << "adcbarstart " << adcbarstart << endl;
+  //T->GetEntry(0);
+  //Int_t adcbarstart = (Int_t)Thodo::ADCBar[0];
+  //cout << "adcbarstart " << adcbarstart << endl;
 
   
   
   //==================================================== Create output root file
   // root file for viewing fits
-  TString outrootfile = ANALYSED_DIR + "/LandauFits_" + InFile + ".root";
+  TString outrootfile = ANALYSED_DIR + "/LandauFits_" + prefix + sRunNum + "_" + DoBar + ".root";
   TFile *f = new TFile(outrootfile, "RECREATE");
 
 
@@ -108,7 +124,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   //==================================================== Create output text file
   // text file for recording fit results
   ofstream landautextfile;
-  TString outtxtfile = ANALYSED_DIR + "/LandauFits_" + InFile + ".txt";
+  TString outtxtfile = ANALYSED_DIR + "/LandauFits_" + prefix + sRunNum + "_" + DoBar + ".txt";
   landautextfile.open(outtxtfile);
 
 
@@ -121,19 +137,25 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
 
   // ADC histos
   TH1F *hADCL[nBars];
-  for(Int_t bar=0; bar<(nBars); bar++){
+  TH1F *hADCR[nBars];
+
+  for(Int_t bar=0; bar<nBars; bar++){
     Int_t b = adcbarstart+bar;
     hADCL[bar] = new TH1F(TString::Format("hADC_Bar%d_L",b),
 			  TString::Format("hADC_Bar%d_L",b),
 			  NAdcBins, AdcBinLow, AdcBinHigh);
-  }
-  TH1F *hADCR[nBars];
-  for(Int_t bar=0; bar<(nBars); bar++){
-    Int_t b = adcbarstart+bar;
+    
     hADCR[bar] = new TH1F(TString::Format("hADC_Bar%d_R",b),
 			  TString::Format("hADC_Bar%d_R",b),
 			  NAdcBins, AdcBinLow, AdcBinHigh);
   }
+  
+  //new histo to look at 2d plot (like online plots)
+  TH2F *hBarVsADC = new TH2F("hBarVsADC","",
+			     NAdcBins, AdcBinLow, AdcBinHigh,
+			     nBars, 0.0+adcbarstart, nAdc+adcbarstart);
+			     
+  
   TH1F *hADCConstantsL = new TH1F("hADCConstantsL",
 				  "Constants of landau fits",
 				  nBars,
@@ -168,33 +190,64 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
 
 
   //================================================================= Event Loop
-  // variables outside event loop
-  Int_t EventCounter = 0;
-
-  // event loop start
   for(Int_t event=0; event<NEventsAnalysis; event++){
     
-    T->GetEntry(event);
-    // cout << "event " << event << endl;
-    EventCounter++;
-    if (EventCounter % 100000 == 0) cout <<
-				      EventCounter << "/" <<
-				      NEventsAnalysis << endl;
-    if(Thodo::NdataAdcBar!=nBars){
-      cout << "ERROR - the number of bars in the sbs-offline root file is not 90"
-	   << endl;
-      exit;
-    }
-    // fill the adc histograms
-    for(Int_t bar=0; bar<nBars; bar++){
-      hADCL[bar]->Fill(Thodo::ADCValL[bar]);
-      hADCR[bar]->Fill(Thodo::ADCValR[bar]);
-    }//bar
+    C->GetEntry(event);
     
+    if (event % 100000 == 0) 
+      cout << event << "/" << NEventsAnalysis << endl;
+    
+    if (DoBar == false){
+      //assumming mapping goes 0-31 L, 32-64 R.
+      Int_t NadcelemID = T->Ndata_bb_hodoadc_adcelemID;
+      //cout << "Number of adc hits: " << NadcelemID << endl;
+      for (Int_t i=0; i<NadcelemID; i++){
+	Int_t adcID = (int)T->bb_hodoadc_adcelemID[i];
+	Double_t a_p = T->bb_hodoadc_a_p[i]; //pedestal corrected integral	    
+	//Double_t a_p = T->bb_hodoadc_a_amp_p[i]; //pedestal corrected amplitude
+ 
+	//cout for debugging
+	//cout << "id: " <<  adcID << " a_p: " << a_p << endl;
+	
+	if (adcID >= 0 && adcID <= 31){ 
+	  hADCL[adcID]->Fill(a_p);
+	  hBarVsADC->Fill(a_p,adcID+adcbarstart);
+	}else if (adcID >= 32 && adcID <= 63){
+	  hADCR[adcID-32]->Fill(a_p);
+	  hBarVsADC->Fill(a_p,adcID+adcbarstart);
+	}
+	else{
+	   cout << "WTF?" << endl;
+	   continue;
+	}
+      }
+    }else if (DoBar == true){
+      Int_t NadcbarID = (int)T->Ndata_bb_hodoadc_bar_adc_id;
+      //cout << "Number of bar entries: " << NadcbarID << endl;
+      for (Int_t i=0; i<NadcbarID; i++){
+	Double_t DadcbarID = T->bb_hodoadc_bar_adc_id[i];
+	Int_t adcbarID = (int)T->bb_hodoadc_bar_adc_id[i];
+	Double_t L_a_p = T->bb_hodoadc_bar_adc_L_ap[i];
+	Double_t R_a_p = T->bb_hodoadc_bar_adc_R_ap[i];
+	
+	//cout  << "id: " << adcbarID << " L_a_p: " << L_a_p << " R_a_p: " << R_a_p <<   endl;
+      
+	if (adcbarID - adcbarstart > 31) {
+	  cout << "Error: adcbarID = " << adcbarID << " is > 31. Before cast to Int_t, value is " << DadcbarID << ". Skipping this event" << endl;
+	  continue;
+	}
+	else{
+	  hADCL[adcbarID]->Fill(L_a_p);
+	  hADCR[adcbarID]->Fill(R_a_p);
+	  hBarVsADC->Fill(L_a_p, adcbarID);
+	  hBarVsADC->Fill(R_a_p, adcbarID);
+	}      
+      }
+    }
   }// event loop
   
 
-
+  //exit(0);
   //==================================================================== Fitting
   //start fitting
   TF1 *fLandauL[nBars];
@@ -293,6 +346,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   Double_t bars[nBars];
   for(Int_t bar=0; bar<(nBars); bar++){
     Int_t b = adcbarstart+bar;
+    
     xerrors[bar] = 0.0;
     bars[bar] = b;
   }
@@ -365,6 +419,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   gWidthR->GetXaxis()->SetTitle("Bar");
   gWidthR->GetYaxis()->SetTitle("width of landau fit");
   gWidthR->SetMarkerColor(kBlue); gWidthR->SetMarkerStyle(21);
+  
   // draw graphs
   // constants
   TCanvas *cConstants = new TCanvas();
@@ -377,6 +432,8 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   lConstants->AddEntry("ConstantsL","Left");
   lConstants->AddEntry("ConstantsR","Right");
   lConstants->Draw();
+  cConstants->Print("temp1.pdf");
+  
   // MPVs
   TCanvas *cMPV = new TCanvas();
   cMPV->SetName("cMPVLeftVRight");
@@ -388,6 +445,8 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   lMPV->AddEntry("MPVL","Left");
   lMPV->AddEntry("MPVR","Right");
   lMPV->Draw();
+  cMPV->Print("temp2.pdf");
+  
   // Widths
   TCanvas *cWidth = new TCanvas();
   cWidth->SetName("cWidthLeftVRight");
@@ -399,9 +458,8 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   lWidth->AddEntry("WidthL","Left");
   lWidth->AddEntry("WidthR","Right");
   lWidth->Draw();
-
-
-
+  cWidth->Print("temp3.pdf");
+  
  //========================================================== Draw Fit Results
   TCanvas *cLeft = new TCanvas("cLeft",
   			       "Left PMTs, ADC spectra",
@@ -412,7 +470,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
     gPad->SetLogy();
     hADCL[b]->Draw();
   }
-
+  
   TCanvas *cRight = new TCanvas("cRight",
   			       "Right PMTs, ADC spectra",
   			       900,700);
@@ -423,8 +481,18 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
     hADCR[b]->Draw();
   }
 
-
-
+  cLeft->Print("temp4.pdf");
+  cRight->Print("temp5.pdf");
+  
+  TCanvas *c2D = new TCanvas("c2D",
+			     "2D Online Plot",
+			     900, 700);
+  c2D->cd();
+  hBarVsADC->GetXaxis()->SetTitle("ADC Value");
+  hBarVsADC->GetYaxis()->SetTitle("ADC ID");
+  hBarVsADC->Draw("colz");
+  c2D->Print("temp6.pdf");
+  
   //=============================================== Write/Draw Histograms/Graphs
   f->cd();
   gDirectory->mkdir("ADC");
@@ -432,6 +500,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   // write the canvases with multiplots
   cLeft->Write();
   cRight->Write();
+  c2D->Write();
   TCanvas *cadc = new TCanvas();
   cadc->cd();
   for(Int_t bar=0; bar<(nBars); bar++){
@@ -440,6 +509,7 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
     hADCR[bar]->GetXaxis()->SetTitle("ADC bin");
     hADCR[bar]->Write();
   }
+  
   f->cd();
   gDirectory->mkdir("FitResults");
   f->cd("FitResults");
@@ -473,16 +543,19 @@ void GetLandau(const TString InFile="bbhodo_307_1000", Int_t nevents=-1,
   gWidthL->Write();
   gWidthR->Write();
 
-
+  gSystem->Exec(Form("pdfunite temp*.pdf LandauPlots_%i_%d.pdf",RunNum,DoBar));
+  gSystem->Exec("rm temp*.pdf");
 
   //========================================================== Close output file
-  f->Close();
+  //f->Close();
   // tidy up canvases
-  delete cConstants;
-  delete cMPV;
-  delete cWidth;
-
-  
+  //delete cConstants;
+  //delete cMPV;
+  //delete cWidth;
+  //delete cadc;
+  //cLeft->Close();
+  //cRight->Close();
+  //cConstants->Close();
 
   //================================================================== End Macro
 }// end macro
