48652d3c3cf25f429636c7e0ce25c872e7d16af6
Garypenman <g.penman.1@research.gla.ac.uk>
2023-04-27 10:06:34 -0400

    Pushing changes to finish hodo calibration at glasgow

--- replay/replayFull/rootlogon.C
+++
@@ -1,9 +0,0 @@
-R__LOAD_LIBRARY($SBS/lib64/libsbs.so)
-// R__LOAD_LIBRARY($SBS/libsbs.so)
-
-void rootlogon(){
-
-// gSystem->AddDynamicPath("${SBS}");
-// gSystem->AddIncludePath(" -I${SBS}");
-
-}
